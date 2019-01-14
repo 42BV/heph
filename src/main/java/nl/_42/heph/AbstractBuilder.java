061047b68684871a2b40db02c4bada677e7f71ad
Arjan Vlek <arjan.vlek@42.nl>
2019-01-14 16:27:20 +0100

    Implemented auto-generation of BuilderConstructors
    AbstractBuilder can now automatically generate the required
    `BuilderConstructors` used to obtain references to a `BuildCommand` given an
    Entity. This is done by accessing the no-args constructor of the entity
    and the BuildCommand class.
    
    For customization and backwards compatibility, it's still possible to
    override the `constructors()` method of `AbstractBuilder` and supply
    your own set of `BuilderConstructors`
    
    Fixes #4

--- src/main/java/nl/_42/heph/AbstractBuilder.java
+++ src/main/java/nl/_42/heph/AbstractBuilder.java
@@ -1,11 +1,19 @@
 package nl._42.heph;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import io.beanmapper.BeanMapper;
 
+import org.springframework.beans.BeanInstantiationException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.GenericTypeResolver;
 import org.springframework.data.domain.Persistable;
+import org.springframework.util.Assert;
+import org.springframework.util.ReflectionUtils;
 
 /**
  * <p>
@@ -27,6 +35,8 @@ import org.springframework.data.domain.Persistable;
  */
 public abstract class AbstractBuilder<T extends Persistable, BC extends AbstractBuildCommand> {
 
+    private BuilderConstructors<T, BC> defaultBuilderConstructors;
+
     /**
      * BeanMapper is used when the copy function is invoked. It will attempt to make a clear
      * copy. Be aware that non-traditional getters/setters may hamper the working of the copy
@@ -36,12 +46,21 @@ public abstract class AbstractBuilder<T extends Persistable, BC extends Abstract
     private BeanMapper beanMapper;
 
     /**
-     * Method which must provide the three constructors; i) constructor for the BuildCommand
+     * Method which provides the three constructors; i) constructor for the BuildCommand
      * if copy/update is called, ii) constructor for the BuildCommand if blank/base is called
      * and iii) constructor for the entity.
+     *
+     * If not overridden, a set of builderConstructors is generated automatically.
+     * See {@link #generateDefaultBuilderConstructors()} for more info on builder construction generation.
      * @return the instance containing the three constructor lambdas.
      */
-    public abstract BuilderConstructors<T, BC> constructors();
+    public BuilderConstructors<T, BC> constructors() {
+        if (defaultBuilderConstructors ==  null) {
+            // Lazily generate builderConstructors the first time they're needed.
+            defaultBuilderConstructors = generateDefaultBuilderConstructors();
+        }
+        return defaultBuilderConstructors;
+    }
 
     /**
      * Returns the entity wrapped in a BuildCommand. All fields which are required for
@@ -142,5 +161,72 @@ public abstract class AbstractBuilder<T extends Persistable, BC extends Abstract
                 throw new RuntimeException("Cannot find id field in the hierarchy.", e);
             }
         }
+    }
+
+    /**
+     * Generates a default pair of builder constructors.
+     * The constructor has three methods, which do the following:
+     * i) constructor for copy/update: Instantiates the buildCommand (using its no-args constructor) with the linked entity
+     * ii) constructor for blank/base: Instantiates the buildCommand (using its no-args constructor) with the supplied entity
+     * iii) constructor for the entity: Instantiates the entity (using its no-args constructor).
+     * @return Builder constructor for the entity type and build command type of this class.
+     */
+    @SuppressWarnings("unchecked")
+    private BuilderConstructors<T, BC> generateDefaultBuilderConstructors() {
+        Class<?>[] genericTypes = GenericTypeResolver.resolveTypeArguments(getClass(), AbstractBuilder.class);
+        Assert.isTrue(genericTypes != null && genericTypes.length == 2, "The AbstractBuilder class must contain exactly two class-level generic types");
+
+        Class<?> entityClass = genericTypes[0];
+        Class<?> buildCommandClass = genericTypes[1];
+
+        // Function 1: Returns a new BuildCommand from an Entity.
+        Function<T, BC> directEntityBuilderFunction = (entity -> instantiateBuildCommand(buildCommandClass, entity));
+
+        // Function 2: Returns a new BuildCommand from a supplied Entity.
+        Function<Supplier<T>, BC> lazySupplyingEntityBuilderFunction = (entity -> instantiateBuildCommand(buildCommandClass, entity.get()));
+
+        // Function 3: Returns a new Entity
+        Supplier<T> entitySupplyingFunction = () -> (T) BeanUtils.instantiateClass(entityClass);
+
+        return new BuilderConstructors<>(directEntityBuilderFunction, lazySupplyingEntityBuilderFunction, entitySupplyingFunction);
+    }
+
+    /**
+     * Instantiates a BuildCommand given its class and an initial Entity
+     * This is done by looking at the no-args constructor (either present in {@link AbstractBuildCommand} or overridden in your own BuildCommand)
+     * @param buildCommandClass Class of the BuildCommand to instantiate
+     * @param entity Entity to set within the "entity" field
+     * @return Instantiated BuildCommand for the given Entity
+     */
+    @SuppressWarnings("unchecked")
+    private BC instantiateBuildCommand(Class<?> buildCommandClass, T entity) {
+        BC buildCommand;
+
+        try {
+            // If the buildCommand is a separate class (that is: not an inner class), directly instantiate it.
+            buildCommand = (BC) BeanUtils.instantiateClass(buildCommandClass);
+        } catch (BeanInstantiationException e) {
+            try {
+                // The buildCommand can be an inner class. In that case, look for the constructor taking its outer class as parameter.
+                Constructor innerClassConstructor = ReflectionUtils.accessibleConstructor(buildCommandClass, this.getClass());
+                buildCommand = (BC) BeanUtils.instantiateClass(innerClassConstructor, this);
+            } catch (NoSuchMethodException e2) {
+                // If the buildCommand cannot be instantiated as regular class or as inner class, throw an exception.
+                throw new BeanInstantiationException(buildCommandClass, "A no-args constructor is required!");
+            }
+        }
+
+        // Once the command has been instantiated, place the entity and "updating" field inside it (this is what the overridable constructors also do).
+        Field entityField = ReflectionUtils.findField(buildCommandClass, "entity");
+        Field updatingField = ReflectionUtils.findField(buildCommandClass, "updating");
+        Assert.isTrue(entityField != null, "BuildCommand class or its superclass does not contain the required field 'entity'");
+        Assert.isTrue(updatingField != null, "BuildCommand class or its superclass does not contain the required field 'updating'");
+        entityField.setAccessible(true);
+        updatingField.setAccessible(true);
+
+        ReflectionUtils.setField(entityField, buildCommand, entity);
+        ReflectionUtils.setField(updatingField, buildCommand, !entity.isNew());
+
+        return buildCommand;
     }
 }
