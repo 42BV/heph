677fb2f76e587714281d988eb218a43248571563
Arjan Vlek <arjan.vlek@42.nl>
2019-03-18 16:24:20 +0100

    Removed hard dependency on BeanMapper
    BeanMapper is only required for the `copy` method, but currently it was
    a hard dependency of this library. To make things worse, its scope was
    `provided`, so you would get a runtime crash if your project did not
    include `BeanMapper` by itself.
    
    This has now been solved. If BeanMapper is *not* on the classpath,
    everything but the `copy` method can still be used.

---
+++ src/test/java/nl/_42/heph/NoBeanMapperTest.java
@@ -0,0 +1,46 @@
+package nl._42.heph;
+
+import static org.assertj.core.api.Fail.fail;
+import static org.junit.Assert.assertEquals;
+
+import nl._42.heph.builder.PersonFixtures;
+import nl._42.heph.domain.Person;
+import nl._42.heph.shared.AbstractSpringTest;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class NoBeanMapperTest extends AbstractSpringTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private PersonFixtures personFixtures;
+
+    /**
+     * This test ensures that the library throws the correct error message if BeanMapper is not on our classpath.
+     */
+    @Test
+    public void noBeanMapperOnClassPath_copyFunctionShouldThrowErrorExplainingHowToResolve() {
+        ClassLoader originalClassLoader = applicationContext.getClassLoader();
+
+        try {
+            ((GenericApplicationContext)applicationContext).setClassLoader(new NoBeanMapperMockClassLoader());
+
+            Person person = personFixtures.sjaak();
+
+            try {
+                personFixtures.copy(person);
+                fail("Expected UnsupportedOperationException was not thrown!");
+            } catch (UnsupportedOperationException e) {
+                assertEquals("The copy feature requires the io.beanmapper dependency to be added to your project. Please include it and then try again.", e.getMessage());
+            }
+        } finally {
+            ((GenericApplicationContext) applicationContext).setClassLoader(originalClassLoader);
+        }
+
+    }
+}
